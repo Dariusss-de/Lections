526f19ce6cef6b8a9293f8dda15686de2af89859
Dmitry <mangosovdima@mail.ru>
2023-01-22 13:13:09 +0300

    added work text

---
+++ Task8/Program.cs
@@ -0,0 +1,32 @@
+﻿// Дан текстю Нужно заменить все пробелы чёрточками, маленькие буквы 'k' заменить на 'K', а большие буквы 'C', заменить 'c'
+string text = "- Я думаю, - сказал князь, улыбаясь - что, "
+              + "ежели бы вас послали вместо нашего милого Винценгероде "
+              + "вы бы взяли приступом согласия прусского короля. "
+              + "Вы так красноречивы. Вы дадите мне час?";
+
+//  string str = "qwerty"
+//                012345
+// str[1] //w
+
+string Replace(string text,char oldValue,char newValue)
+{
+    string result = String.Empty;
+    int Length = text.Length;
+    for(int i=0;i<Length;i++)
+    {
+        if(text[i] == oldValue) result =  result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+   
+
+
+    return result;
+}
+string newText = Replace(text,' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText,'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText,'с', 'С');
+Console.WriteLine(newText);
