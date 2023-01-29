fd21b0393712a8104f2f9cec5ceb9f7f0a58fd9e
Dmitry <mangosovdima@mail.ru>
2023-01-29 13:38:05 +0300

    added task4

---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿//Приветствия определённого пользователя уникально
+Console.Write("Введите имя пользователя: ");
+string name = Console.ReadLine();
+switch(name)
+{
+    case "Маша":
+    Console.WriteLine("Добрый день, "+name);
+    break;
+    case "Антон":
+    Console.WriteLine("Добрый день, господин "+name);
+    break;
+}
+
+/*
+Console.Write("Введите имя пользователя: ");
+string name = Console.ReadLine();
+if(name.ToLower() == "Маша") Cosole.WriteLine("Добрый день, "+name);
+else if (name.ToLower() == "Паша") Cosole.WriteLine("Добрый день,господин "+name);
+*/
