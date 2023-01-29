500c9165e9b9cc08c736d54f6583c59678f58c36
Dmitry <mangosovdima@mail.ru>
2023-01-29 13:37:16 +0300

    added task2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Нахождение суммы чисел
+Console.Write("Введите первое число: ");
+double num1 = double.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double num2 = double.Parse(Console.ReadLine());
+double result = num1+num2;
+Console.WriteLine("Сумма чисел = "+result);
+
+/*
+int num1 = new Random().Next(1,100);
+int num2 = new Random().Next(1,100);
+int result = num1+num2;
+Console.WriteLine("Сумма чисел = "+result);
+*/
