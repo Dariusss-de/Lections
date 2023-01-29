f980d53e7fcd4e3dc7d41849a5717c9484082b6f
Dmitry <mangosovdima@mail.ru>
2023-01-29 13:37:45 +0300

    added task3

---
+++ Task3/Program.cs
@@ -0,0 +1,7 @@
+﻿//Нахождение частного числа чисел
+Console.Write("Введите первое число: ");
+double num1 = double.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double num2 = double.Parse(Console.ReadLine());
+double result = num1/num2;
+Console.WriteLine("Сумма чисел = "+result);
