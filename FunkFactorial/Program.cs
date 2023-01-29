5c941f4685e046e31ad3b42779dcbe829bb36c8f
Dmitry <mangosovdima@mail.ru>
2023-01-29 13:35:54 +0300

    added factorial

---
+++ FunkFactorial/Program.cs
@@ -0,0 +1,14 @@
+﻿// Нахождение факториала
+
+double Factorial (int fact)
+{
+    // 1! =1
+    // 0! =1
+    if(fact == 1 || fact == 0) return 1;
+    else return fact * Factorial (fact -1) ;
+}
+
+
+Console.Write("Введите значение факториала: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Значение факториала числа {num} = {Factorial (num)}");
